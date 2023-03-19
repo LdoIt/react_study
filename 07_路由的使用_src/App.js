78ff02ab6dc6e0b79094a814ff5d4d3f66c283f7
Good <1204889603@qq.com>
2023-03-19 20:50:14 +0800

    Basic usage of routes

--- 07_路由的使用_src/App.js
+++ 07_路由的使用_src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Link, Route } from 'react-router-dom'
-import About from './components/About'
-import Home from './components/Home'
+import { NavLink, Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
 export default class App extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
         <div>
@@ -19,12 +20,12 @@ export default class App extends Component {
               <div className="list-group">
                 {/* <a className="list-group-item active" href="./about.html">About</a>
                 <a className="list-group-item" href="./home.html">Home</a> */}
-                <Link className="list-group-item" to="/about">
+                <NavLink activeClassName="selected" className="list-group-item" to="/about">
                   About
-                </Link>
-                <Link className="list-group-item" to="/home">
+                </NavLink>
+                <NavLink activeClassName="selected" className="list-group-item" to="/home">
                   Home
-                </Link>
+                </NavLink>
               </div>
             </div>
             <div className="col-xs-6">
--- 07_路由的使用_src/index.js
+++ 07_路由的使用_src/index.js
@@ -3,4 +3,4 @@ import reactDomClient from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 const root = reactDomClient.createRoot(document.getElementById('root'))
-root.render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>)
+root.render(<BrowserRouter><App/></BrowserRouter>)
